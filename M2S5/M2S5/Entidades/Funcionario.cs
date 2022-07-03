b71338acb156fb8e3044588c11d69ac8ed121d4c
Natanael Vieira <natanaelvieira130@gmail.com>
2022-07-03 14:56:11 -0300

    M2S5 - Exercicio 05

---
+++ M2S5/M2S5/Entidades/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿namespace M2S5.Entidades {
+    public class Funcionario {
+
+        public string nome { get; set; } 
+        public string id { get; set; }
+
+        public Funcionario() { }
+
+        public Funcionario(string nome,string id) {
+            this.nome = nome;
+            this.id = id;   
+        }
+    }
+}
--- M2S5/M2S5/Program.cs
+++ M2S5/M2S5/Program.cs
@@ -44,40 +44,57 @@ public class Program {
         #endregion
 
         #region Exercicio 2
-        Cartao cartao = new Cartao();
-        cartao.valorTaxa = 2;
-        cartao.saldo = 100;
-        cartao.bandeira = "visa";
+        //Cartao cartao = new Cartao();
+        //cartao.valorTaxa = 2;
+        //cartao.saldo = 100;
+        //cartao.bandeira = "visa";
 
-        CartaoVA cartaoVA = new CartaoVA();
-        cartaoVA.bandeira = "mastercard";
-        cartaoVA.saldo = 200;
-        cartaoVA.valorTaxa = 3;
+        //CartaoVA cartaoVA = new CartaoVA();
+        //cartaoVA.bandeira = "mastercard";
+        //cartaoVA.saldo = 200;
+        //cartaoVA.valorTaxa = 3;
 
-        CartaoVR cartaoVR = new CartaoVR();
-        cartaoVR.valorTaxa = 4;
-        cartaoVR.bandeira = "visa";
-        cartaoVR.saldo = 300;
+        //CartaoVR cartaoVR = new CartaoVR();
+        //cartaoVR.valorTaxa = 4;
+        //cartaoVR.bandeira = "visa";
+        //cartaoVR.saldo = 300;
 
-        CartaoVT cartaoVT = new CartaoVT();
-        cartaoVT.bandeira = "mastercard";
-        cartaoVT.valorTaxa = 5;
-        cartaoVT.saldo = 400;
+        //CartaoVT cartaoVT = new CartaoVT();
+        //cartaoVT.bandeira = "mastercard";
+        //cartaoVT.valorTaxa = 5;
+        //cartaoVT.saldo = 400;
 
-        Console.WriteLine($"Taxa Cartao: {cartao.VerificarValorTaxa(cartao.saldo,cartao.valorTaxa)}");
-        Console.WriteLine($"Taxa CartaoVA: {cartaoVA.VerificarValorTaxa(cartaoVA.saldo,cartaoVA.valorTaxa)}");
-        Console.WriteLine($"Taxa CartaoVR: {cartaoVR.VerificarValorTaxa(cartaoVR.saldo,cartaoVR.valorTaxa)}");
-        Console.WriteLine($"Taxa CartaoVT: {cartaoVT.VerificarValorTaxa(cartaoVT.saldo,cartaoVT.valorTaxa)}");
+        //Console.WriteLine($"Taxa Cartao: {cartao.VerificarValorTaxa(cartao.saldo,cartao.valorTaxa)}");
+        //Console.WriteLine($"Taxa CartaoVA: {cartaoVA.VerificarValorTaxa(cartaoVA.saldo,cartaoVA.valorTaxa)}");
+        //Console.WriteLine($"Taxa CartaoVR: {cartaoVR.VerificarValorTaxa(cartaoVR.saldo,cartaoVR.valorTaxa)}");
+        //Console.WriteLine($"Taxa CartaoVT: {cartaoVT.VerificarValorTaxa(cartaoVT.saldo,cartaoVT.valorTaxa)}");
 
 
         #endregion
 
         #region Exercicio 3
 
-        Cartao cartaoEx3 = new CartaoVA();
-        CartaoVA cartao2Ex3 = new Cartao();
+        //Cartao cartaoEx3 = new CartaoVA();
+        //CartaoVA cartao2Ex3 = cartaoEx3;
 
         #endregion
 
+
+        #region Exercicio 5
+
+        Funcionario funcionario = new Funcionario();
+        funcionario = null;
+        try {
+            ImprimeNomeFuncionario(funcionario);
+        } catch(Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
+        #endregion
+    }
+
+    private static void ImprimeNomeFuncionario(Funcionario func) {
+        if(func == null)
+            throw new NullReferenceException("O objeto e nulo");
+        Console.WriteLine(func.nome);
     }
 }
